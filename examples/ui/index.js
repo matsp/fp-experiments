c0e92333c4bb161215c62c9b068835ecf98599a5
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-09-16 20:10:16 +0200

    Create event listeners

--- examples/ui/index.js
+++ examples/ui/index.js
@@ -3,9 +3,15 @@ const h = (...args) => {
   const attrsKeys = Object.keys(attrs) || []
   const el = document.createElement(tag)
   attrsKeys.forEach(k => {
-      const a = document.createAttribute(k)
-      a.value = attrs[k]
-      el.setAttributeNode(a)
+      if (k.startsWith('on')) {
+        const event = k.substring(2).toLowerCase()
+        el.addEventListener(event, attrs[k])
+      }
+      else {
+        const a = document.createAttribute(k)
+        a.value = attrs[k]
+        el.setAttributeNode(a)
+      }
   })
   children.forEach(c => {
     c instanceof Node 
@@ -15,12 +21,12 @@ const h = (...args) => {
   return el
 }
 
+console.time()
 const test = h('div', {class: 'test', style: 'color: red;'}, 
                 h('span', {style: 'color: blue;'}, 'nested blue text'),
-                h('h1', {}, 'Title'),
+                h('h1', {onClick: () => console.log('clicked')}, 'Title'),
              'red text')
 
-console.time()
 const mountpoint = document.getElementById('app')
 mountpoint.appendChild(test)
 console.timeEnd()
