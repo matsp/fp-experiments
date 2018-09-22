b4e772cac2cff2e1c48df1ca788f61bbbfe0a469
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-09-22 11:52:21 +0200

    Backup commit.

--- examples/ui/index.js
+++ examples/ui/index.js
@@ -1,32 +1,57 @@
-const h = (...args) => {
-  const [tag, attrs, ...children] = args
-  const attrsKeys = Object.keys(attrs) || []
-  const el = document.createElement(tag)
-  attrsKeys.forEach(k => {
-      if (k.startsWith('on')) {
-        const event = k.substring(2).toLowerCase()
-        el.addEventListener(event, attrs[k])
+import { h } from './utils.js'
+
+const state = {
+    data: () => {
+      let res = []
+      for (let i = 0; i<=1000;i++) {
+        res.push(Math.random().toString(36).substring(2, 15))
       }
-      else {
-        const a = document.createAttribute(k)
-        a.value = attrs[k]
-        el.setAttributeNode(a)
-      }
-  })
-  children.forEach(c => {
-    c instanceof Node 
-      ? el.appendChild(c)
-      : el.appendChild(document.createTextNode(c))
-  })
-  return el
+      return res
+    }
+  }
+
+const createOberservable = (observable, {onGet, onSet}) => {
+  const interceptor = {
+    get (target, key) {
+      onGet(key)
+      return target[key]
+    },
+    set (target, prop, value) {
+      onSet(prop, value)
+      return true
+    }
+  }
+  return new Proxy(observable, interceptor)
 }
 
-console.time()
-const test = h('div', {class: 'test', style: 'color: red;'}, 
-                h('span', {style: 'color: blue;'}, 'nested blue text'),
-                h('h1', {onClick: () => console.log('clicked')}, 'Title'),
-             'red text')
+const obj = {
+  title: 'hello'
+}
+
+const store = createOberservable(obj, {
+  onGet(key) {
+    console.log(`calling get on ${key}`)
+  },
+  onSet(prop, value) {
+    console.log(`calling set on ${prop} with value ${value}`)
+  }
+})
+
+console.log(store.title)
+
+store.title = 'title'
+
+
+
+console.time('rendering 1000 nodes')
+
+const listComponent = state => {
+  const list = children => h('ul', {}, ...children)
+  const data = state.data().map(d => h('li', {style: 'color: red;'}, d))
+  return list(data)
+}
 
 const mountpoint = document.getElementById('app')
-mountpoint.appendChild(test)
-console.timeEnd()
+mountpoint.appendChild(listComponent(state))
+
+console.timeEnd('rendering 1000 nodes')
