9d825b7c8ffc2ce47ce3b6674819659e61228fb7
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-09-28 10:57:34 +0200

    Refactor in fns and add simple reactivity.

--- examples/ui/index.js
+++ examples/ui/index.js
@@ -1,57 +1,29 @@
-import { h } from './utils.js'
+import { h, createOberservable, updateDOM } from './utils.js'
 
 const state = {
-  data: () => {
-    let res = []
-    for (let i = 0; i <= 1000; i++) {
-      res.push(Math.random().toString(36).substring(2, 15))
-    }
-    return res
-  }
+  // data: () => {
+  //   let res = []
+  //   for (let i = 0; i <= 1000; i++) {
+  //     res.push(Math.random().toString(36).substring(2, 15))
+  //   }
+  //   return res
+  // }
+  data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 }
 
-const createOberservable = (target, listener) => {
-  let observable
-  const handler = {
-    get (target, key) {
-      return target[key]
-    },
-    set (target, prop, value) {
-      target[prop] = value
-      listener(observable)
-      return true
-    }
-  }
-  observable = new Proxy(target, handler)
-  return observable
-}
-
-const initialState = {
-  title: 'hello'
-}
-
-const store = createOberservable(initialState, {
-  onGet (key) {
-    console.log(`calling get on ${key}`)
-  },
-  onSet (prop, value) {
-    console.log(`calling set on ${prop} with value ${value}`)
-  }
-})
-
-console.log(store.title)
-
-store.title = 'title'
-
-console.time('rendering 1000 nodes')
-
+// rendering fn
 const listComponent = state => {
   const list = children => h('ul', {}, ...children)
-  const data = state.data().map(d => h('li', { style: 'color: red;' }, d))
+  const data = state.data.map(d => h('li', { style: 'color: red;' }, d))
   return list(data)
 }
 
-const mountpoint = document.getElementById('app')
-mountpoint.appendChild(listComponent(state))
+// customized functions
+const updateMountpoint = updateDOM('app')
+const updateComponents = updateMountpoint(listComponent)
+const stateObservable = createOberservable({ target: state, listener: updateComponents })
 
-console.timeEnd('rendering 1000 nodes')
+// first render
+console.time(`rendering ${state.data.length} nodes`)
+updateComponents(stateObservable)
+console.timeEnd(`rendering ${state.data.length} nodes`)
--- examples/ui/utils.js
+++ examples/ui/utils.js
@@ -13,3 +13,25 @@ export const h = (tag, attrs, ...children) => {
   })
   return el
 }
+
+export const createOberservable = ({ target, listener }) => {
+  let observable
+  const handler = {
+    get (target, key) {
+      return target[key]
+    },
+    set (target, prop, value) {
+      target[prop] = value
+      listener(observable)
+      return true
+    }
+  }
+  observable = new Proxy(target, handler)
+  return observable
+}
+
+export const updateDOM = rootID => (...renderingFns) => state => {
+  const rootNode = document.getElementById(rootID)
+  // TODO: cache rendered nodes
+  renderingFns.forEach(r => rootNode.appendChild(r(state)))
+}
