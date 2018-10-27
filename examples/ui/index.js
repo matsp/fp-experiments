49ec09f6a6322c24047a42e352e06d947a96e3d6
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-10-27 22:13:51 +0200

    Manual rerender.

--- examples/ui/index.js
+++ examples/ui/index.js
@@ -1,6 +1,6 @@
-import { h, createObservable, _createObservable, updateDOM } from './utils.js'
+import { h, updateDOM } from './utils.js'
 
-const initialState = {
+let initialState = {
   data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 }
 
@@ -15,25 +15,25 @@ const buttonComponent = state => {
   return h('button', {
     onClick: () => {
       state.data.push('99')
-      console.log(state)
     }
   }, 'add')
 }
 
+const renderComponent = state => {
+  return h('button', {
+    onClick: () => {
+      console.time(`update state -> rerender`)
+      updateComponents(state)
+      console.timeEnd(`update state -> rerender`)
+    }
+  }, 'render')
+}
+
 // customized functions
 const updateMountpoint = updateDOM('app')
-const updateComponents = updateMountpoint(listComponent, buttonComponent)
-const stateObservable = _createObservable(initialState)
-stateObservable.subscribe(newValue => updateComponents(newValue))
-// const stateObservable = createObservable({ target: initialState, listener: updateComponents })
-// stateObservable(7)
-updateComponents(stateObservable())
-// first render
-// console.log(stateObservable)
-// console.time(`first rendering`)
-// updateComponents(stateObservable)
-// console.timeEnd(`first rendering`)
+const updateComponents = updateMountpoint(listComponent, buttonComponent, renderComponent)
 
-// console.time(`update state -> rerender`)
-// stateObservable.data.push('99')
-// console.timeEnd(`update state -> rerender`)
+// first render
+console.time(`first rendering`)
+updateComponents(initialState)
+console.timeEnd(`first rendering`)
--- examples/ui/utils.js
+++ examples/ui/utils.js
@@ -17,55 +17,14 @@ export const h = (tag, attrs, ...children) => {
   return el
 }
 
-// TODO: Better way instead of global storage?!
-const proxies = new WeakSet()
-/**
- * Make the given target obj reactive by calling the listener on changes
- */
-export const createObservable = ({ target, listener }) => {
-  let observable
-  const handler = {
-    get (target, key) {
-      if (typeof target[key] === 'object' && target[key] !== null && !proxies.has(target[key])) {
-        const newObservable = createObservable({ target: target[key], listener: listener })
-        target[key] = newObservable
-        proxies.add(newObservable)
-      }
-      return target[key]
-    },
-    set (target, key, value) {
-      target[key] = value
-      listener(observable)
-      return true
-    }
-  }
-  observable = new Proxy(target, handler)
-  proxies.add(observable)
-  return observable
-}
-
 /**
  * Custom dom updating function creation
  */
 export const updateDOM = rootID => (...renderingFns) => state => {
   const rootNode = document.getElementById(rootID)
+  while (rootNode.firstChild) {
+    rootNode.removeChild(rootNode.firstChild)
+  }
   const rendered = renderingFns.map(r => r(state))
   rendered.forEach(node => rootNode.appendChild(node))
-}
-
-export const _createObservable = value => {
-  const listeners = []
-  const notify = newValue => listeners.forEach(listener => listener(newValue))
-
-  const observer = newValue => {
-    if (newValue && newValue !== value) {
-      value = newValue
-      notify(newValue)
-      console.log(`called notify on all listeners with value: ${newValue}`)
-    }
-    return value
-  }
-  observer.subscribe = listener => listeners.push(listener)
-
-  return observer
 }
