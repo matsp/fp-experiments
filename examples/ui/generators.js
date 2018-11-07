ead6061a191aeccf3086577c2ef7c69d7c8db7bd
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2018-11-07 20:01:52 +0100

    Export pipeline operators.

--- examples/ui/generators.js
+++ examples/ui/generators.js
@@ -1,7 +1,3 @@
-// test promises
-const arrayPromise = () => new Promise(resolve => setTimeout(() => resolve([1, 2, 3, 4, 5]), 500))
-const delayPromise = () => new Promise(resolve => setTimeout(() => resolve(), 1000))
-
 /**
  * Pipeline functions with dispatch injection.
  */
@@ -10,7 +6,7 @@ export const pipe = (...fns) => (x, dispatch) => fns.reduce((v, f) => f(v, dispa
 /**
  * Map for async iterators.
  */
-const map = mapper => async function * (iterable) {
+export const map = mapper => async function * (iterable) {
   for await (const item of iterable) {
     yield mapper(item)
   }
@@ -19,7 +15,7 @@ const map = mapper => async function * (iterable) {
 /**
  * Filter for async iterators.
  */
-const filter = predicate => async function * (iterable) {
+export const filter = predicate => async function * (iterable) {
   for await (const item of iterable) {
     yield predicate(item)
   }
@@ -33,16 +29,18 @@ const filter = predicate => async function * (iterable) {
 export const runPipeline = async (pipeline) => {
   let callback = null
   const queue = []
-  const producer = (async function * producer () {
+
+  const actions = (async function * () {
     while (true) {
       if (queue.length > 0) yield queue.shift()
       await new Promise(resolve => { callback = resolve })
       callback = null
     }
   }())
+
   const dispatch = action => {
     if (callback) callback()
     queue.push(action)
   }
-  for await (const action of pipeline(producer, dispatch)) {}
+  for await (const action of pipeline(actions, dispatch)) {}
 }
